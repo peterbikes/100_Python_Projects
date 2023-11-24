af349888fce835440c1b887849f4c9ebb2a410de
pedro-mota <pedrosmmota@gmail.com>
2023-11-24 22:53:57 +0000

    hangman added

--- 10 - Hangman/hangman.py
+++ 10 - Hangman/hangman.py
@@ -32,32 +32,30 @@ def hangman():
     guessed = False
     letters = [];
     tries = 15
-    os.system("clear")
     while(True):
-        guessed = check_guessed(letters, word);
         if(guessed):
             print("Word is", word.upper() + ", you won!")
             return;
         if(tries == 0):
             print("Word was", word.upper() + ", you LOST!!")
             return;
+        os.system("clear")
         print ("Hangman 💀")
         print("You have", tries, "tries left:")
         print("Letters tried:", letters)
-        user_letter = input("Give us a letter please: ")
-        os.system("clear")
-        if(len(user_letter) == 1 and user_letter.isalpha()):
-            letters.append(user_letter.lower())
-        else:
-            print("Stupid attempd, let's go ahead and deduct a try anyway.")
         for letter in word:
             if(letter in letters):
                 print(letter.upper(), end=" ")
             else:
                 print("_", end=" ")
+        user_letter = input("\nGive us a letter please: ")
+        while (len(user_letter) != 1 or user_letter.isalpha() == False):
+            user_letter = input("Stupid attempd, try again: ")
+        letters.append(user_letter.lower())
         if(user_letter not in word):
             tries -= 1
-        print("\n")
-
+        guessed = check_guessed(letters, word);
+        if(guessed == False and tries > 0):
+            os.system("clear")
 
 hangman()
